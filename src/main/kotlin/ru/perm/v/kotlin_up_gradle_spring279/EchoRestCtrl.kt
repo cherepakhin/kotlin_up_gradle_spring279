b50615b6a4c18c06c69dda065af937b3bec6d037
Черепахин Василий <vasi.che@gmail.com>
2023-03-15 14:24:30 +0500

    Создан EchoRestCtrl.kt, EchoRestCtrlTest.kt

---
+++ src/main/kotlin/ru/perm/v/kotlin_up_gradle_spring279/EchoRestCtrl.kt
@@ -0,0 +1,13 @@
+package ru.perm.v.kotlin_up_gradle_spring279
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class EchoRestCtrl {
+    @GetMapping("/echo/{mes}")
+    fun echo(@PathVariable("mes") mes: String): String {
+        return mes
+    }
+}
