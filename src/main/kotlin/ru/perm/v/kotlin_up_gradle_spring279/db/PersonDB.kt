d5f2263e07359c269887f8af5703b714ac5d3c81
Черепахин Василий <vasi.che@gmail.com>
2023-03-16 15:36:34 +0500

    Исправление ошибки "Data class must have at least one primary constructor parameter"

--- src/main/kotlin/ru/perm/v/kotlin_up_gradle_spring279/db/PersonDB.kt
+++ src/main/kotlin/ru/perm/v/kotlin_up_gradle_spring279/db/PersonDB.kt
@@ -6,15 +6,10 @@ import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
-@Table(name="person")
-data class PersonDB {
+@Table(name = "person")
+data class PersonDB(
     @Id
-    var id: Int = 0
+    var id: Int = 0,
     @Column(name = "name", length = 40)
     var name: String = ""
-
-    constructor(id: Int, name: String) {
-        this.id = id
-        this.name = name
-    }
-}
+)
