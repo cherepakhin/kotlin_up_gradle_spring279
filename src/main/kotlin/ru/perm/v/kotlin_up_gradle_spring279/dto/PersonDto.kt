2d0ded6eb4b896130731fa0ae7920072e14e11f0
Черепахин Василий <vasi.che@gmail.com>
2023-03-16 12:35:08 +0500

    Swagger описание для PersonDto.kt

--- src/main/kotlin/ru/perm/v/kotlin_up_gradle_spring279/dto/PersonDto.kt
+++ src/main/kotlin/ru/perm/v/kotlin_up_gradle_spring279/dto/PersonDto.kt
@@ -5,7 +5,24 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Model for PersonDto")
 data class PersonDto( // data!!!
+    @field:Schema(
+        description = "Identificator",
+        example = "1,2,3...",
+        type = "int",
+        minimum = "0",
+        defaultValue = "0"
+    )
     var id: Int = 0,
-    var name: String = "",
+    @field:Schema(
+        description = "Name of person",
+        example = "Ivan,Petr,...",
+        defaultValue = "-"
+    )
+    var name: String = "-",
+    @field:Schema(
+        description = "Sex of person",
+        example = "MEN/WOMEN",
+        defaultValue = "MEN"
+    )
     var sex: Sex = Sex.MEN
 )
