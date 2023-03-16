16eaded35614b4cf278e96e891c1051a20102e5c
Черепахин Василий <vasi.che@gmail.com>
2023-03-16 15:39:42 +0500

    Исправление ошибки "Data class must have at least one primary constructor parameter"

--- src/main/kotlin/ru/perm/v/kotlin_up_gradle_spring279/db/PersonDB.kt
+++ src/main/kotlin/ru/perm/v/kotlin_up_gradle_spring279/db/PersonDB.kt
@@ -12,4 +12,22 @@ data class PersonDB(
     var id: Int = 0,
     @Column(name = "name", length = 40)
     var name: String = ""
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PersonDB
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + name.hashCode()
+        return result
+    }
+}
